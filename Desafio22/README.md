0c0aa5af641029d418584918bb49f448dc501bfc
Alexzx396 <a.l.e.x_91@hotmail.com>
2022-07-07 23:09:30 -0400

    Desafio DENO y desafio framework

---
+++ Desafio22/README.md
@@ -0,0 +1,10 @@
+store-api
+a Sails v1 application
+
+Links
+Sails framework documentation
+Version notes / upgrading
+Deployment tips
+Community support options
+Professional / enterprise options
+Version info
---
+++ Desafio23/server/src/helpers/color.helpers.ts
@@ -0,0 +1,40 @@
+import { Context } from "../../deps.ts";
+import { Color } from "../types/color.type.ts";
+
+const DB_COLOR: Color[] = [];
+
+export const findAll =  (ctx: Context) => {
+  try {
+    ctx.response.status = 200;
+    console.info(`status: ${ctx.response.status}, method: findAll handler`);
+    ctx.response.body =  { code: "00", data: DB_COLOR };
+  } catch (error) {
+    ctx.response.status = 500;
+
+    console.error(`status: ${ctx.response.status}, ${error}`);
+  }
+};
+
+export const save = async (ctx: Context) => {
+  try {
+    ctx.response.status = 201;
+    console.info(`status: ${ctx.response.status}, method: save handler`);
+
+    const { colorName } = await ctx.request.body().value;
+
+    const color: Color = {
+      colorName: colorName,
+    };
+    
+    DB_COLOR.push(color);
+
+    ctx.response.body = { code: "00", data: color };
+  } catch (error) {
+    ctx.response.status = 500;
+
+    console.error(`status: ${ctx.response.status}, ${error}`);
+  }
+};
+
+Footer
+© 2022 GitHub,
