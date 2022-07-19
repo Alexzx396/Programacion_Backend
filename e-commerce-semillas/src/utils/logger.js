308e6dab105e0d6405e0e8fa45d24c2bcd2fea98
Alexzx396 <a.l.e.x_91@hotmail.com>
2022-07-19 14:36:24 -0400

    Añadir ecommerce semillas

---
+++ e-commerce-semillas/src/utils/logger.js
@@ -0,0 +1,39 @@
+const winston = require("winston");
+const config = require("./config.js");
+const path = require("path");
+
+function buildProdLogger() {
+  const prodLogger = winston.createLogger({
+    transports: [
+      new winston.transports.File({
+        filename: path.join(path.dirname(""), "./logs/warn.log"),
+        level: "warn",
+      }),
+      new winston.transports.File({
+        filename: path.join(path.dirname(""), "./logs/error.log"),
+        level: "error",
+      }),
+      new winston.transports.Console({
+        format: winston.format.combine(winston.format.colorize(), winston.format.simple())
+      })
+    ],
+  });
+  return prodLogger;
+}
+
+function buildDevLogger() {
+  const devLogger = winston.createLogger({
+    transports: [new winston.transports.Console({ level: "info" })],
+  });
+  return devLogger;
+}
+
+let logger = null;
+
+if (config.srv.NODE_ENV === "PROD") {
+  logger = buildProdLogger();
+} else {
+  logger = buildDevLogger();
+}
+
+module.exports = logger;
