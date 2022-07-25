61098cfbbe5cb47fbc60192836f92ea9c44c9b0b
Alexzx396 <a.l.e.x_91@hotmail.com>
2022-07-25 18:32:27 -0400

    borrar Proyecto final

--- ProyectoFinal Backend/src/classes/CustomError.class.js
+++
@@ -1,9 +0,0 @@
-class CustomError {
-    constructor(code, description, detail) {
-      this.code = code;
-      this.description = description;
-      this.detail = detail;
-    }
-  }
-  
-  export default CustomError;
